41b356399fbab35cd233a70350daea520e82b540
alechp <me@alechp.com>
2022-09-25 12:34:53 -0700

    added repogen config and gitignore

---
+++ .repogen.js
@@ -0,0 +1,10 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    { alechp: "dx-ide" },
+    { alechp: "dx-cli" },
+  ],
+};
+
+module.exports = { config };
